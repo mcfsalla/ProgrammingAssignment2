1b2c1df36fb5c9606e686492665a65344bffcf95
mcfsalla <37814742+mcfsalla@users.noreply.github.com>
2018-03-26 18:30:53 -0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Function makeCacheMatrix creates a matrix and cache its inverse for future use.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv_mat <- NULL
+    set <- function(y) {
+        x <<- y
+        inv_mat <<- NULL
+    }
+    
+    get = function() x
+    set_inv_mat = function(inverse) inv_mat <<- inverse
+    get_inv_mat = function() inv_mat
+    list(set = set, get = get, set_inv_mat = set_inv_mat, get_inv_mat = get_inv_mat)
 }
 
+## Function cacheSolve computes the ouput of function makeCacheMatrix according to two constraints:
+## 1) If the matrix has been calculated, it retrieves the output and skips the computation;
+## 2) Otherwise, it calculates the inverse of the data and computes the inverse matrix.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { ## output of function MakeCacheMatrix
+    inv = x$get_inv_mat() ## Return the inverse matrix of 'x'
+    if (!is.null(inv_mat)) { ## if the inverse matrix has already been calculated
+        message("cached data")
+        return(inv_mat) ## retrieves it from the cache and skips the computation (saving time and processing) 
+    }
+    
+    mat_data = x$get() ## otherwise, calculates the inverse matrix
+    inv_mat = solve(mat_data, ...)
+    x$set_inv_mat(inv_mat)
+    return(inv_mat)
 }
